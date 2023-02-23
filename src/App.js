8b37108838eb0d8909103dd18d7c4aaf5d920999
agonzalez <agonzalez.develop@gmail.com>
2023-02-23 19:05:34 +0100

    Un compoñente con dous botons e valores asignados aos botons

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { useState } from 'react';
 function App() {
 
   let [Valor, SetValor] = useState(0)
+  let [ValorIncremento, SetValorIncremento] = useState(0)
+  let [ValorDisminucion, SetValorDisminucion] = useState(0)
 
   function ClickAumentar () {
     let Aumentar = Valor + 1
@@ -14,11 +16,23 @@ function App() {
     SetValor(Disminuir)
   }
 
+  function NumeroDeIncrementos () {
+    const NumeroTotalIncrementos = ValorIncremento + 1
+    SetValorIncremento(NumeroTotalIncrementos)
+    ClickAumentar()
+  }
+
+  function NumeroDeDisminucion () {
+    const NumeroTotalDisminucion = ValorDisminucion - 1
+    SetValorDisminucion(NumeroTotalDisminucion)
+    ClickDisminuir()
+  }
+
   return (
     <>
     <h1>{Valor}</h1>
-    <button onClick={ClickAumentar}>Aumentar</button>
-    <button onClick={ClickDisminuir}>Disminuir</button>
+    <button onClick={NumeroDeIncrementos}>+{ValorIncremento}</button>
+    <button onClick={NumeroDeDisminucion}>-{ValorDisminucion}</button>
     </>
   );
 }
