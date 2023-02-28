4e265a00a3a47e8f86cace2dd4bb6820c0a6879b
agonzalez <agonzalez.develop@gmail.com>
2023-02-28 20:07:36 +0100

    Novas Funcionalidades engadidas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react';
 import Boton from './components/boton.jsx';
-import NovoBoton from './components/NovoBoton.jsx';
 
 function App() {
 
   let [Valor, SetValor] = useState(0)
   let [Auto, SetAuto] = useState(false)
+  let [Turbo, SetTurbo] = useState(false)
+  let [HiperTurbo, SetHiperTurbo] = useState(false)
+  let [UltraTurbo, SetUltraTurbo] = useState(false)
 
   function NumeroDeIncrementos () {
     let NumeroTotalIncrementos = Valor + 1
@@ -22,9 +24,26 @@ function App() {
   }
 
   function PararContador() {
-    SetAuto(false)
+    SetAuto(false) || SetTurbo(false) || SetHiperTurbo(false)  || SetUltraTurbo(false) 
   }
-  
+
+  function ContadorTurbo() {
+    SetTurbo(true)
+  }
+
+  function ContadorHiperTurbo() {
+    SetHiperTurbo(true)
+  }
+
+  function ContadorUltraTurbo() {
+    SetUltraTurbo(true)
+  }
+
+  function Reiniciar() {
+    let Reinicio = 0
+    SetValor(Reinicio)
+  }
+
   useEffect(
       ()=> {
       let Tempo  
@@ -34,13 +53,44 @@ function App() {
     },
   )
 
+  useEffect(
+    ()=> {
+      let SuperTempo
+      if (Turbo) {
+        SuperTempo = setTimeout (NumeroDeIncrementos, 100)}
+        return ()=> {clearTimeout(SuperTempo)}
+    },
+  )
+
+  useEffect(
+    ()=> {
+      let HiperTempo
+      if (HiperTurbo) {
+        HiperTempo = setTimeout (NumeroDeIncrementos, 10)}
+        return ()=> {clearTimeout(HiperTempo)}
+    },
+  )
+
+  useEffect(
+    ()=> {
+      let UltraTempo
+      if (UltraTurbo) {
+        UltraTempo = setTimeout (NumeroDeIncrementos, 1)}
+        return ()=> {clearTimeout(UltraTempo)}
+    },
+  )
+
   return (
     <>
       <h1>{Valor}</h1>
       <Boton texto='+' operador={NumeroDeIncrementos}/>
       <Boton texto='-' operador={NumeroDeDisminucion}/>
-      <NovoBoton texto='Auto' operador={ContadorAuto}/>
-      <NovoBoton texto='stop' operador={PararContador}/>
+      <Boton texto='Auto' operador={ContadorAuto}/>
+      <Boton texto='stop' operador={PararContador}/>
+      <Boton texto='Turbo' operador={ContadorTurbo}/>
+      <Boton texto='Hiper Turbo' operador={ContadorHiperTurbo}/>
+      <Boton texto='Ultra Turbo' operador={ContadorUltraTurbo}/>
+      <Boton texto='Reiniciar' operador={Reiniciar}/>
     </>
   );
 }
