fc259ac6f4b1166b03b1566c72f24bac46c541af
agonzalez <agonzalez.develop@gmail.com>
2023-03-02 20:44:32 +0100

    Entrega de exercicio para avaliación

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
 import Boton from './components/boton.jsx';
 import NovoBoton from './components/NovoBoton.jsx';
-import styles from './App.css';
 
 function App() {
 
@@ -11,34 +10,34 @@ function App() {
   let [HiperTurbo, SetHiperTurbo] = useState(false)
   let [UltraTurbo, SetUltraTurbo] = useState(false)
 
-  function NumeroDeIncrementos () {
+  function NumeroDeIncrementos() {
     let NumeroTotalIncrementos = Valor + 1
     SetValor(NumeroTotalIncrementos)
   }
 
-  function NumeroDeDisminucion () {
+  function NumeroDeDisminucion() {
     const NumeroTotalDisminucion = Valor - 1
     SetValor(NumeroTotalDisminucion)
   }
 
-  function ContadorAuto () {
-    SetAuto(true)
-  }
-
-  function PararContador() {
-    SetAuto(false) || SetTurbo(false) || SetHiperTurbo(false)  || SetUltraTurbo(false) 
+  function ContadorAuto() {
+    SetAuto(true) || SetTurbo(false) || SetHiperTurbo(false) || SetUltraTurbo(false)
   }
 
   function ContadorTurbo() {
-    SetTurbo(true)
+    SetTurbo(true) || SetAuto(false) || SetHiperTurbo(false) || SetUltraTurbo(false)
   }
 
   function ContadorHiperTurbo() {
-    SetHiperTurbo(true)
+    SetHiperTurbo(true) || SetAuto(false) || SetTurbo(false) || SetUltraTurbo(false)
   }
 
   function ContadorUltraTurbo() {
-    SetUltraTurbo(true)
+    SetUltraTurbo(true) || SetAuto(false) || SetTurbo(false) || SetHiperTurbo(false)
+  }
+
+  function PararContador() {
+    SetAuto(false) || SetTurbo(false) || SetHiperTurbo(false) || SetUltraTurbo(false)
   }
 
   function Reiniciar() {
@@ -46,62 +45,70 @@ function App() {
     SetValor(Reinicio)
   }
 
-  useEffect (
-      ()=> {
-      let Tempo 
-      if (Auto) { 
-      Tempo = setTimeout (NumeroDeIncrementos, 1000)}
-      return ()=> {clearTimeout(Tempo)}
+  useEffect(
+    () => {
+      let Tempo
+      if (Auto) {
+        Tempo = setTimeout(NumeroDeIncrementos, 1000)
+      }
+      return () => { clearTimeout(Tempo) }
     },
   )
 
   useEffect(
-    ()=> {
+    () => {
       let SuperTempo
       if (Turbo) {
-        SuperTempo = setTimeout (NumeroDeIncrementos, 100)}
-        return ()=> {clearTimeout(SuperTempo)}
+        SuperTempo = setTimeout(NumeroDeIncrementos, 100)
+      }
+      return () => { clearTimeout(SuperTempo) }
     },
   )
 
   useEffect(
-    ()=> {
+    () => {
       let HiperTempo
       if (HiperTurbo) {
-        HiperTempo = setTimeout (NumeroDeIncrementos, 10)}
-        return ()=> {clearTimeout(HiperTempo)}
+        HiperTempo = setTimeout(NumeroDeIncrementos, 10)
+      }
+      return () => { clearTimeout(HiperTempo) }
     },
   )
 
   useEffect(
-    ()=> {
+    () => {
       let UltraTempo
       if (UltraTurbo) {
-        UltraTempo = setTimeout (NumeroDeIncrementos, 1)}
-        return ()=> {clearTimeout(UltraTempo)}
+        UltraTempo = setTimeout(NumeroDeIncrementos, 1)
+      }
+      return () => { clearTimeout(UltraTempo) }
     },
   )
 
   return (
-    <>
-      <h1>{Valor}</h1>
-      <div className={styles.App} class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
-        <div class="btn-group mr-2" role="group" aria-label="First group">
-          <Boton texto='+' operador={NumeroDeIncrementos} type="primary" />
-          <Boton texto='-' operador={NumeroDeDisminucion} type="secondary" />
+    <body>
+      <div class="card">
+        <div class="card-body">
+          <h1>{Valor}</h1>
         </div>
-        <div class="btn-group mr-2" role="group" aria-label="Second group">
-          <NovoBoton texto='Auto' operador={ContadorAuto} type="info" />
-          <NovoBoton texto='Turbo' operador={ContadorTurbo} type="info" />
-          <NovoBoton texto='Super Turbo' operador={ContadorHiperTurbo} type="info" />
-          <NovoBoton texto='Ultra Turbo' operador={ContadorUltraTurbo} type="info" />
-        </div>
-        <div>
-          <NovoBoton texto='STOP' operador={PararContador} type="danger" />
-          <NovoBoton texto='Reiniciar' operador={Reiniciar} type="warning" />
+        <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+          <div class="btn-group mr-2" role="group" aria-label="First group">
+            <Boton texto='+ ' operador={NumeroDeIncrementos} type="primary" />
+            <Boton texto='- ' operador={NumeroDeDisminucion} type="secondary" />
+          </div>
+          <div class="btn-group mr-2" role="group" aria-label="Second group">
+            <NovoBoton texto='auto' operador={ContadorAuto} type="info" />
+            <NovoBoton texto='turbo' operador={ContadorTurbo} type="info" />
+            <NovoBoton texto='super turbo' operador={ContadorHiperTurbo} type="info" />
+            <NovoBoton texto='ultra turbo' operador={ContadorUltraTurbo} type="info" />
+          </div>
+          <div class="btn-group mr-2" role="group" aria-label="Third group">
+            <NovoBoton texto='stop' operador={PararContador} type="danger" />
+            <NovoBoton texto='reiniciar' operador={Reiniciar} type="warning" />
+          </div>
         </div>
       </div>
-    </>
+    </body>
   );
 }
 
